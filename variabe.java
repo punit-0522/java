e2ea3b2557fe80631438c2caa55ca38d7261a2d1
punit-0522 <punitjaiswal785621@gmail.com>
2025-08-26 23:53:29 +0530

    Add files via upload

---
+++ variabe.java
@@ -0,0 +1,16 @@
+public class variabe {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum =a+b;
+    System.out.println("ADDITION:"+sum);
+int diff = a-b;
+System.out.println("SUBTRACTION:"+diff);
+int mul = a*b;
+System.out.println("MULTIPLICATION:"+mul);
+int div =a/b;
+System.out.println("DIVISION:"+div);
+int mod =a%b;
+System.out.println("MODULUS:"+mod);
+    }
+}
