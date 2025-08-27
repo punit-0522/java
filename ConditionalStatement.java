e5481e0ab8770a0483751044db294400f019c911
punit-0522 <punitjaiswal785621@gmail.com>
2025-08-27 23:17:03 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class conditional {
+
+    public static void main(String[] args)
+    {
+       // Scanner sc =new Scanner(System.in);      //{input in java}
+        // String name = sc.nextLine();                    
+        //  System.out.println(name);   
+
+
+
+//      Scanner sc = new Scanner(System.in);
+//         int a= sc.nextInt();
+//         int b= sc.nextInt();
+//     int sum=a+b;
+// System.out.println(sum);    }
+
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+if (a==b) {
+    System.out.println("equal");
+}
+else if (a>b) {
+    System.out.println("a is greater");
+    }
+else{
+   System.out.println( "a is lesser");
+}
+
+
+
+// int button =sc.nextInt();
+// switch (button) {
+//     case 1:System.out.println("hello");
+//         break;
+//         case 2:System.out.println("namaste");
+//         break;case 3:System.out.println("bonjour");
+//         break;
+//     default:System.out.println("invalid button");
+//         break;
+    
+// }
+}
+}
+
+
