519f8b81c60d4425208b67ce9af859c9fc25df09
punit-0522 <punitjaiswal785621@gmail.com>
2025-09-03 15:25:45 +0530

    Add files via upload

---
+++ TimeComplexity.java
@@ -0,0 +1,46 @@
+// BEST CASE: O(1)
+// AVERAGE CASE:O(n+1/2)
+// WORST CASE:O(n)
+
+import java.util.Scanner;
+
+public class TimeComplexity {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println("hello world");
+            }
+        }
+    }
+}
+// TIME COMPLEXITY : (n*n times)=O(n^2)
+
+
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.println("hello");
+            }
+        }
+    }
+    // TIME COMPLEXITY: O(n*m)
+
+
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        for (int i=0;i<n;i++){
+        System.out.println("hello");
+     }
+            for(int j=0;j<m;j++){
+                System.out.println("world");
+            }
+    }
+// TIME COMPLEXITY:O(n+m)
